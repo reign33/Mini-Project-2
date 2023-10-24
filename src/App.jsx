ce35dce742e645fb38e404968134021cf7168da3
larrconcepcion <95194385+BADTRY@users.noreply.github.com>
2023-10-24 20:08:47 +0800

    larry's work

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
 import React from "react"
+<<<<<<< HEAD
 import Footer from "./components/Footer"
 import LoginPage from "./components/LoginPage"
 import "./index.css"
 
+=======
+import Navbar from "./Components/Navbar"
+import "./index.css"
+>>>>>>> f39a1bf (larry's work)
 
 function App() {
   return (
     <>
+<<<<<<< HEAD
     <LoginPage />
     <Footer />
+=======
+      <Navbar/>
+>>>>>>> f39a1bf (larry's work)
     </>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -10,14 +10,3 @@ export default {
   plugins: [],
 }
 
-// const defaultTheme = require('tailwindcss/defaultTheme')
-
-// module.exports = {
-//   theme: {
-//     extend: {
-//       fontFamily: {
-//         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
-//       },
-//     },
-//   },
-// }
