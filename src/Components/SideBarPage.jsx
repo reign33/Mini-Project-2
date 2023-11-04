4caa279a1d53305f0e2aff36d8673860bf117012
Ron Cley <124509262+reign33@users.noreply.github.com>
2023-11-04 23:42:45 +0800

    nov 4 sidebar ui

---
+++ src/Components/SideBarPage.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import { Sidebar, Menu, MenuItem, SubMenu, menuClasses } from 'react-pro-sidebar';
+import { AiFillHome, AiFillAppstore, AiOutlinePlusSquare, AiOutlineInbox, AiOutlineShoppingCart, AiOutlineShop } from "react-icons/ai";
+import {FaChartBar} from "react-icons/fa";
+
+function SideBarPage() {
+  return (
+    <div style={{ display: 'flex', height: '100vh'}}>
+  
+    <Sidebar className='bg-amazonclone'>
+      <div className='pl-[60px] pt-[30px] h-[100px] bg-amazonclone'>
+        <img src="public/amazon_logo.png" alt="amazon logo" className='w-[110px]'/>
+      </div>
+      <Menu
+        rootStyles={{
+          [`.${menuClasses.icon}`]: {
+            backgroundColor: 'black',
+            color: 'white',
+          },
+          backgroundColor:"black",
+          color: "white",
+          borderTop: "0.5px solid white"
+          
+        }}
+      >
+        <MenuItem active icon={<AiFillHome/>} className='hover:text-black'>Dashboard</MenuItem>
+        <SubMenu label="Category" icon={<AiFillAppstore/>} className='my-[10px] hover:text-[red]'>
+          <MenuItem icon={<AiOutlinePlusSquare/>} className='bg-black text-white hover:text-black'> Add Category</MenuItem>
+          <MenuItem icon={<AiOutlineInbox/>} className='bg-black text-white hover:text-black' > All Category</MenuItem>
+          <MenuItem icon={<FaChartBar/>} className='bg-black text-white hover:text-black' > Reports</MenuItem>
+        </SubMenu>
+        <MenuItem active icon={<AiOutlineShop/>} className='my-[10px] hover:text-black'>
+          Products
+        </MenuItem>
+        <MenuItem active icon={<AiOutlineShoppingCart/>} className='mt-[10px] hover:text-black'>
+          Orders
+        </MenuItem>
+      </Menu>
+      <div className='bg-black h-[40vh]'></div>
+    </Sidebar>
+    
+  </div>
+  )
+}
+
+export default SideBarPage;
--- src/components/LoginPage.jsx
+++ src/components/LoginPage.jsx
@@ -1,8 +1,24 @@
-import React from 'react';
-import { useNavigate } from 'react-router-dom'
+import React, {useState} from 'react';
+import { useNavigate } from 'react-router-dom';
 
-export default function LoginPage() {
-    
+export default function LogInForm() {
+    const [details, setDetails] = useState({email:"", password:""});
+
+    const adminUser = {
+        email: "admin@admin.com",
+        password: "admin123"
+      }
+
+    const submitHandler = e => {
+        e.preventDefault();
+        console.log(details);
+        if(details.email == adminUser.email && details.password == adminUser.password){
+            navigate('/')
+        } else {
+            setDetails({email:"", password:""});
+        }
+    }
+
     const navigate = useNavigate();
     function handleBackToSignUp(){
         navigate('/signup')
@@ -12,30 +28,32 @@ export default function LoginPage() {
     <div class="m-0 p-0 box-border">
     <div class="container w-1/2 m-auto">
         <div class="logo w-32 block m-auto">
-            <img class="m-3" src="/images.png" alt="" srcSet="" />
+            <img class="m-3" src="/images.png" alt="Amazon Logo" srcSet="" />
         </div>
         {/* <!-- logo end --> */}
         <div class="loginform w-1/2 m-auto border-[2px] rounded-lg border-gray-200 p-[20px]">
             <h1 class="font-sans text-black font-normal text-3xl mb-2">Sign In</h1>
 
+            <form onSubmit={submitHandler}>
             <p class="font-semibold text-sm my-[10px]">
-                Email or mobile phone number
-            </p>
+                Email or mobile phone number</p>
             <input
                 class="focus:border-[3px] outline-none focus:border-blue-200
                 border-[1px] rounded-[3px] border-[#a6a6a6] w-[100%] px-[1px] py-[1px]"
-                type="text" name="username" id="" placeholder="" />
-                <p class="font-semibold text-sm my-[10px]">
-                Password
-            </p>
+                type="text" name="email"
+                onChange={e => setDetails({...details, email: e.target.value})} value={details.email}/>
+                <p class="font-semibold text-sm my-[10px]">Password</p>
             <input
                 class="focus:border-[3px] outline-none focus:border-blue-200
-                border-[1px] rounded-[3px] border-[#a6a6a6] w-[100%] px-[1px] py-[1px] mb-[10px]"
-                type="password" name="username" id="" placeholder="" />
+                border-[1px] rounded-[3px] border-[#a6a6a6] w-[100%] px-[1px] py-[1px]"
+                type="password" name="password" id="password"
+                onChange={e => setDetails({...details, password: e.target.value})} value={details.password}
+                />
 
             <button class="mt-[12px] border rounded-lg py-[5px] text-sm w-[100%] bg-[#f8db02] hover:bg-[#ebd004]" type="submit">
                 Continue
             </button>
+            </form>
             <p class="text-xs font-medium mt-3 mb-2 text-gray-800 p-1">
                 By continuing, you agree to Amazon's 
                 <a class="cursor-pointer hover:text-red-700 hover:underline text-[#0066c0]" href="/"> Contitions of
