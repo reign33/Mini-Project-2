3e581072f0232fab185647ad1395bfde28152a5f
Ron Cley <124509262+reign33@users.noreply.github.com>
2023-11-08 17:54:07 +0800

    update nov 8 rc

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
-import React, { useState } from "react"
+import React from "react"
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./index.css"
-import {HomePage, SearchResults, ProductPage, Checkout, LoginPage, SignUpPage, SideBarPage} from "./Components";
-
-
+import {HomePage, SearchResults, ProductPage, Checkout, LoginPage, SignUpPage, SideBarPage, SidebarAddCategory} from "./Components";
 
 function App() {
 
@@ -11,12 +9,14 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route exact path="/" element={<HomePage />} />
-        <Route path="/search" element={<SearchResults />} />
+        <Route path="/search" element={<SearchResults/>} />
         <Route path="/product/:id" element={<ProductPage />} />
         <Route path="/checkout" element={<Checkout />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<SignUpPage />} />
-        <Route path="/sidebar" element={<SideBarPage />} />
+        <Route path="/sidebar" element={<SideBarPage />}>
+          <Route path="addcategory" element={<SidebarAddCategory/>} />
+        </Route>
       </Routes>
     </BrowserRouter>
   )
--- src/Components/SideBarPage.jsx
+++ src/Components/SideBarPage.jsx
@@ -2,12 +2,13 @@ import React from 'react'
 import { Sidebar, Menu, MenuItem, SubMenu, menuClasses } from 'react-pro-sidebar';
 import { AiFillHome, AiFillAppstore, AiOutlinePlusSquare, AiOutlineInbox, AiOutlineShoppingCart, AiOutlineShop } from "react-icons/ai";
 import {FaChartBar} from "react-icons/fa";
+import { Link } from 'react-router-dom';
 
 function SideBarPage() {
   return (
     <div style={{ display: 'flex', height: '100vh'}}>
   
-    <Sidebar className='bg-amazonclone'>
+    <Sidebar backgroundColor="black">
       <div className='pl-[60px] pt-[30px] h-[100px] bg-amazonclone'>
         <img src="/amazon_logo.png" alt="amazon logo" className='w-[110px]'/>
       </div>
@@ -25,21 +26,20 @@ function SideBarPage() {
       >
         <MenuItem active icon={<AiFillHome/>} className='hover:text-black'>Dashboard</MenuItem>
         <SubMenu label="Category" icon={<AiFillAppstore/>} className='my-[10px] hover:text-[red]'>
-          <MenuItem icon={<AiOutlinePlusSquare/>} className='bg-black text-white hover:text-black'> Add Category</MenuItem>
+          <MenuItem component={<Link to="addcategory"/>} icon={<AiOutlinePlusSquare/>} className='bg-black text-white hover:text-black'> Add Category</MenuItem>
           <MenuItem icon={<AiOutlineInbox/>} className='bg-black text-white hover:text-black' > All Category</MenuItem>
           <MenuItem icon={<FaChartBar/>} className='bg-black text-white hover:text-black' > Reports</MenuItem>
         </SubMenu>
-        <MenuItem active icon={<AiOutlineShop/>} className='my-[10px] hover:text-black'>
-          Products
-        </MenuItem>
+        <SubMenu label="Reports" icon={<AiOutlineShop/>} className='my-[10px] hover:text-[red]'>
+          <MenuItem icon={<AiOutlinePlusSquare/>} className='bg-black text-white hover:text-black'> Add Product</MenuItem>
+          <MenuItem icon={<AiOutlineInbox/>} className='bg-black text-white hover:text-black' > All Product</MenuItem>
+          <MenuItem icon={<FaChartBar/>} className='bg-black text-white hover:text-black' > Reports</MenuItem>
+        </SubMenu>
         <MenuItem active icon={<AiOutlineShoppingCart/>} className='mt-[10px] hover:text-black'>
           Orders
         </MenuItem>
       </Menu>
-      <div className='bg-black h-[40vh]'></div>
     </Sidebar>
-    
-    
   </div>
   )
 }
