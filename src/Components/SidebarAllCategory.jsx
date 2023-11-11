7c499f17053f957abdd8b3bc96eb851f25cf1e1f
Ron Cley <124509262+reign33@users.noreply.github.com>
2023-11-11 23:00:26 +0800

    update 11pm ng gabi

--- src/Components/SidebarAllCategory.jsx
+++ src/Components/SidebarAllCategory.jsx
@@ -8,7 +8,7 @@ function SidebarAllCategory() {
     <div className='flex'>
       <SideBarPage />
 
-      <div className='flex flex-col items-center border w-[180vh]'>
+      <div className='flex flex-col items-center border w-[182vh]'>
         <div className='flex justify-between items-center w-[160vh] h-[15vh]'>
           <div>
             <div className='flex'>
@@ -45,7 +45,43 @@ function SidebarAllCategory() {
 
         <div className='flex justify-between item-center w-[160vh] mt-[20px] text-[14px] text-[gray] font-medium'>
           <p>0 of 7 Selected</p>
-          <div>Carousel page by number</div>
+
+          {/* Pagination */}
+          <div>
+<nav aria-label="Page navigation example">
+  <ul class="flex items-center -space-x-px h-8 text-sm">
+    <li>
+      <a href="#" class="flex items-center justify-center px-3 h-8 ms-0 leading-tight text-gray-500 bg-white border border-e-0 border-gray-300 rounded-s-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
+        <span class="sr-only">Previous</span>
+        <svg class="w-2.5 h-2.5 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+          <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 1 1 5l4 4"/>
+        </svg>
+      </a>
+    </li>
+    <li>
+      <a href="#" class="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">1</a>
+    </li>
+    <li>
+      <a href="#" class="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">2</a>
+    </li>
+    <li>
+      <a href="#" aria-current="page" class="z-10 flex items-center justify-center px-3 h-8 leading-tight text-[white] rounded-lg border border-[#EC6E45] bg-[#FF9900] hover:bg-[#EC6E45] dark:border-gray-700 dark:bg-gray-700 dark:text-white">3</a>
+    </li>
+
+    <li>
+      <a href="#" class="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 rounded-e-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
+        <span class="sr-only">Next</span>
+        <svg class="w-2.5 h-2.5 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+          <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4"/>
+        </svg>
+      </a>
+    </li>
+  </ul>
+</nav>
+
+
+          </div>
+
           <div className='text-black'>
             <button className='w-[100px] bg-[#F0F0F0] mr-[20px] rounded-lg p-[5px] active:bg-[gray] active:text-[white]' type="button">previous</button>
             <button className='w-[100px] bg-[#F0F0F0] rounded-lg p-[5px] active:bg-[gray] active:text-[white]' type="button">Next</button>
