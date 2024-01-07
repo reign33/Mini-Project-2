9c006f5eb0fdff3db947df3b031ac9e022d3a741
Ron Cley <124509262+reign33@users.noreply.github.com>
2024-01-08 04:21:53 +0800

    db

--- src/utilities/CallApi.jsx
+++ src/utilities/CallApi.jsx
@@ -1,17 +1,4 @@
-// import axios from "axios";
-// import { BASE_URL } from "./constants";
 
-// const config = {
-//   headers: {
-//     "Content-Type": "application/json",
-//     Accept: "application/json",
-//   },
-// };
-
-// export const callAPI = async (resource) => {
-//   const { data } = await axios.get(`${BASE_URL}/${resource}`, config);
-//   return data;
-// };
 
 import axios from "axios";
 import { BASE_URL } from "./constants";
